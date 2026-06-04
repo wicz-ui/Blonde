3b924ac4cc5f169d232e0b8072147eb0b81673b2
silvajuniordorival-eng <silva.junior.dorival@escola.pr.gov.br>
2026-06-03 22:00:02 -0300

    (NÃO TERMINADO AINDA) CÓDIGO DO APP MOBILE
    ESTE O CÓDIGO COMPLETO DO FRONT END  DO TELEMÓVEL.

---
+++ Protótipos/silvajuniordorival-eng/app.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect }
+from 'React';
+import {StylesSheet, Text, View, TouchableOpacity, Alert, ActivityIndicator} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import QRCode from 'react-native-qrcode-svg';
+
+// Altere para o IP da sua máquina onde o servidor Flask está rodando na rede local
+const API_URL = 'http://192.168.1.100:5000/api/obter-tokens-offline';
+const CPF_TESTE = '000.000.000-00';
+
+export default function App() {
+    const [tokenAtual, setTokenAtual] = useState('');
+    const [carregando, setCarregando] = useState(false);
+    const [statusRede, setStatusRede] = useState('Online');
+
+    // Função para sincronizar novos tokens enquanto há internet
+    const sincronizarTokensParaUsoOffline = async() => {
+        setCarregando(true);
+        try {
+            const resposta = await fetch(API_URL, {
+                method: 'POST',
+                headers: { 'Content-Type': 'applicattion/json'},
+                boby: JSON. stringify({cpf: CPF_TESTE}),
+            });
+            if (resposta.status === 200) { const dados = await resposta.json();
+                // Salva o lote de tokens recebido de forma segura na memória interna do telemóvel
+                await AsyncStorage.setItem('@lote_tokens_catraca', JSON.stringify(dados.tokens));
+                // Define o primeiro token ativo na tela
+                setTokenAtual(dados.tokens[0]);
+                setStatusRede('Sincronizado (Pronto para Offline)');
+                Alert.alert('Sucesso', 'Tokens offline atualizados! Você pode acessar mesmo sem rede.')}
+                else { throw new Error ('Falha no servidor');}
+            } catch (error) {
+                // Se falhar (ex:sem internet), o aplicativo entra em MODO OFFLINE automaticamente
+                setStatusRede('Modo Offline Ativo');
+                recuperarTokenDoArmazenamentoLocal();
+            } finally {
+                setCarregando(false);
+            }
+        };
+        // Função executada quando o telemóvel está sem internet para ler a memória local
+        const recuperarTokenDoArmazenamentoLocal = async () => {
+            try {
+                const tokensSalvos = await AsyncStorage.getItem('@lote_tokens_catraca');
+                if (tokensSalvos !== null) {
+                    const listaTokens = JSON.parse(tokensSalvos);
+
+                    if (tokensSalvos !== null) {
+                        const
+                    }
+                }
+            }
+        }
+            )
+        }
+    }
+}
